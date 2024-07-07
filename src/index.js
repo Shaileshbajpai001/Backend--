b9e0d9fc5a5bef6437fa0923dd75075879037bba
Shailesh Bajpai <shaileshshaileshbajpai4488@gmail.com>
2024-07-07 11:35:56 +0530

    connect database

---
+++ src/index.js
@@ -0,0 +1,64 @@
+// require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv"
+
+import mongoose from "mongoose"
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:"./env"
+})
+
+connectDB()  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app =express()
+
+( async()=>{   //iife function [execute imediately] using syntax
+
+    try {
+        
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+  console.log("ERROR404: ",error);
+  throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.log("ERROR",error);
+        throw error
+    }
+} )()                    
+
+*/
